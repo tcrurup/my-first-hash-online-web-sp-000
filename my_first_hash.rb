f3104d8cd80569be8ff23c1fff18300b6217b81f
Anthony Rurup <tcrurup@gmail.com>
2019-04-28 07:12:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,7 @@
 def my_hash
   "name" => "Tony Rurup"
-  "height" => "6'5"
+  "height" => "6'5\""
+  "weight_lbs" => 215
 
   
 end
@@ -10,6 +11,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  the_manifest
 
 
 end
